2ec57bee883685fef4b9fbcd6d64ec4e7f973044
Melquisedeque <melck_junior@hotmail.com>
2023-03-28 15:31:16 +0100

    navbar component layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductCardComponent
+    ProductCardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
