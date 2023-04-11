12ef4bc50840fe64831b9756d97faf95208a7a22
Melquisedeque <melck_junior@hotmail.com>
2023-04-11 17:03:17 +0100

    category card component custom attributes

--- src/app/components/category-card/category-card.component.ts
+++ src/app/components/category-card/category-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-category-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-card.component.css']
 })
 export class CategoryCardComponent implements OnInit {
+@Input() cardImg: string = '';
+@Input() cardTitle: string = '';
+
+
+
 
   constructor() { }
 
