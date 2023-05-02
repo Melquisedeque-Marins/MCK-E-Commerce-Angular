43ccbef91ec94a948a5fc8b552c997ed1ccec9cb
Melquisedeque <melck_junior@hotmail.com>
2023-05-02 10:41:17 +0100

    conditional flow after cart

--- src/app/components/order-sumary/order-sumary.component.ts
+++ src/app/components/order-sumary/order-sumary.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Address } from 'src/app/models/Address';
 import { Cart } from 'src/app/models/Cart';
+import { AddressService } from 'src/app/services/address.service';
 import { CartService } from 'src/app/services/cart.service';
 
 @Component({
@@ -8,13 +10,21 @@ import { CartService } from 'src/app/services/cart.service';
   styleUrls: ['./order-sumary.component.css']
 })
 export class OrderSumaryComponent implements OnInit {
-  
+  address!: boolean;
   cart!: Cart;
-  constructor(private cartService: CartService) {
+
+  constructor(private cartService: CartService,
+              private addressService: AddressService) {
     this.cartService.getCartObservable().subscribe((cart) => {
       this.cart = cart;
-    })
+    });
+    this.addressService.getDeliveryAddressObservable().subscribe((address) => {
+      
+      this.address = address ? true : false;
+    });
    }
+
+
   ngOnInit(): void {
   }
 
